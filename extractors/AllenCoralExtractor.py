30220c67cae0b26329d7564fc71db4867405b5df
Daniel Schauder <danschauder@gmail.com>
2021-11-13 16:03:12 -0500

    Updated folder structure

---
+++ extractors/AllenCoralExtractor.py
@@ -0,0 +1,9 @@
+class AllenCoralExtractor():
+    def __init__(self):
+        """
+        Extracts data from the Allen Coral Atlas API
+        """
+        pass
+
+    def test(self):
+        print('this is a test')
---
+++ main.py
@@ -0,0 +1,8 @@
+from extractors.AllenCoralExtractor import AllenCoralExtractor
+
+def main():
+    extractor = AllenCoralExtractor()
+    extractor.test()
+
+if __name__=="__main__":
+    main()
